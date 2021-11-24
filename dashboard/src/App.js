d3583619a03a9e380d17e7cfe116476bf7f965e1
arafatiqbal <arafatiqbal123@gmail.com>
2021-11-23 17:02:34 -0800

    Pushing working files

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
