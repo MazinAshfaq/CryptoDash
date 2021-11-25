ff60a742095e2bfeb656b1cdba64ef996c1ead16
arafatiqbal <arafatiqbal123@gmail.com>
2021-11-25 13:42:49 -0800

    Footer Updated

---
+++ dashboard/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer id="footer" className="footer">
+      <a
+        className="footerLink "
+        href="https://github.com/ntommy06/CS410P-FinalProject"
+      >
+        Github
+      </a>
+      <p id="copright"></p>
+      <p id="copyright">© 2021 Arafat Iqbal and Mazin Ashfaq.</p>
+    </footer>
+  );
+}
+
+export default Footer;
