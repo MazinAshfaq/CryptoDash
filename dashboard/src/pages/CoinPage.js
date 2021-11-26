45c34460d5afdfa43431916dfe7b054bd05ed3b5
Mazin Ashfaq <mzash00@gmail.com>
2021-11-25 19:17:54 -0800

    saving progess

---
+++ dashboard/src/pages/CoinPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CoinPage() {
+  return (
+    <div className="CoinPage">
+      <p>Coinpage</p>
+    </div>
+  );
+}
+
+export default CoinPage;
--- dashboard/src/pages/Home.js
+++ dashboard/src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function Home() {
   return (
-    <div className="home">
-      <p>Hellow World Please Work</p>
-    </div>
+    <>
+      <div className="home">
+        <p>Hellow World Please Work</p>
+      </div>
+    </>
   );
 }
 
