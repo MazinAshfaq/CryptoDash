450e989c4abed5a6c05f07bf499e43e4c8bf766a
arafatiqbal <arafatiqbal123@gmail.com>
2021-11-27 22:31:52 -0800

    adding the missing files

---
+++ dashboard/src/pages/Coin.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Coin.css";
+
+const coin = ({ name, image, symbol, price, volume, priceChange }) => {
+  return (
+    <div className="coin-container">
+      <div className="coin-row">
+        <div className="coin">
+          <img src={image} alt="crypto" />
+          <h1>{name}</h1>
+          <p className="coin-symbol">{symbol}</p>
+        </div>
+        <div className="coin-data">
+          <p className="coin-price">
+            <b>Price: </b> ${price.toLocaleString()}
+          </p>
+          <p className="coin-volume">
+            <b>Volume: </b> ${volume.toLocaleString()}
+          </p>
+          <p>
+            <b>% Change</b>
+          </p>
+          {priceChange < 0 ? (
+            <p className="coin-percent red"> {priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className="coin-percent green"> {priceChange.toFixed(2)}%</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default coin;
