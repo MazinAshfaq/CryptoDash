6821e9d2aa1cb5f21fe16823bda30c5eec7ca0d2
Mazin Ashfaq <mzash00@gmail.com>
2021-11-30 23:28:38 -0800

    intial trading page

---
+++ dashboard/src/pages/Trading.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Trading.css";
+const Trading = () => {
+  return (
+    <div className="tGrid">
+      <div className="tList"></div>
+    </div>
+  );
+};
+
+export default Trading;
