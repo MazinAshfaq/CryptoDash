c38e84c5672c84259170e4c20df03ff848372cd6
arafatiqbal <arafatiqbal123@gmail.com>
2021-12-01 00:16:11 -0800

    Contact page done

--- dashboard/src/pages/ContactUs.js
+++ dashboard/src/pages/ContactUs.js
@@ -1,10 +1,47 @@
-import React from "react";
-function ContactUs() {
+import React, { useState, useEffect } from "react";
+import "./ContactUs.css";
+
+const ContactUs = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    console.log(e);
+  };
+
   return (
-    <div className="contactus">
-      <p>Contact Us Page</p>
+    <div className="ContactPage">
+      <div className="ContactUs">
+        <form className="contact-form" onSubmit={handleSubmit}>
+          <h1>Contact Us</h1>
+
+          <label>Name</label>
+          <input
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <label>Email</label>
+          <input
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <label>Message</label>
+          <textarea
+            placeholder="Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default ContactUs;
